c6253834b8e2484eb4956e762d8400372d446b0b
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2019-10-12 13:27:00 -0600

    Add BUZZ1 and BUZZ2 dataset loading to buzz_data_maker; create stub for buzz_ann

--- buzz_data_maker.py
+++ buzz_data_maker.py
@@ -17,6 +17,20 @@ BEE = np.array([1, 0, 0])
 CRICKET = np.array([0, 1, 0])
 NOISE = np.array([0, 0, 1])
 
+# relative paths; we assume that everything is running in the top-level Bee_Recognizer directory
+current_dir = os.getcwd()
+datasets = os.path.join(current_dir, 'datasets')
+# buzz1 paths
+buzz1 = os.path.join(datasets, 'BUZZ1')
+buzz1_training = os.path.join(buzz1, 'training')
+buzz1_testing = os.path.join(buzz1, 'testing')
+# BUZZ1 does not have a validation group
+
+# buzz2 paths
+buzz2 = os.path.join(datasets, 'BUZZ2')
+buzz2_training = os.path.join(buzz2, 'training')
+buzz2_testing = os.path.join(buzz2, 'testing')
+buzz2_validation = os.path.join(buzz2, 'validation')
 
 #### Miscellaneous functions
 # save and load pickle functions grabbed from hw02
@@ -45,3 +59,27 @@ def traverse_directory_for_sounds(path, buzz_setting, data):
             wav = np.array(load_sound(full_path_file))
             data.append((wav, buzz_setting))
     return data
+
+def load_buzz_dataset(path):
+    sound_data = []
+    labels = []
+    data = (sound_data, labels)
+    bee_path = os.path.join(path, 'bee')
+    cricket_path = os.path.join(path, 'cricket')
+    noise_path = os.path.join(path, 'noise')
+    data = traverse_directory_for_images(bee_path, BEE, data) 
+    data = traverse_directory_for_images(cricket_path, CRICKET, data) 
+    data = traverse_directory_for_images(noise_path, NOISE, data) 
+    return data
+
+def load_buzz1():
+   training = load_buzz_dataset(buzz1_training)
+   testing = load_buzz_dataset(buzz1_testing)
+   return training, testing
+
+def load_buzz2():
+   training = load_buzz_dataset(buzz2_training)
+   testing = load_buzz_dataset(buzz2_testing)
+   validation = load_buzz_dataset(buzz2_validation)
+   return training, testing, validation
+
